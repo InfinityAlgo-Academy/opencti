b7bd2c861ba8b365b17c770706461ac6deeaa31d
Romy Alula <romy.alula@filigran.io>
2024-01-23 17:56:49 +0100

    [frontend] add utils folder to vitest config (improvement/frontend-unit-testing)

--- opencti-platform/opencti-front/vitest.config.ts
+++ opencti-platform/opencti-front/vitest.config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     setupFiles: 'src/__tests__/setup-relay-for-vitest.ts',
-    include: ['src/__tests__/**/**/*.test.{ts,tsx}']
+    include: [
+      'src/__tests__/**/**/*.test.{ts,tsx}',
+      'src/utils/**/**/*.test.{ts,tsx}'
+    ]
   },
 })
