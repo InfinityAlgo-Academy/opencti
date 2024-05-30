5913c42d700ba71021a2f6f3bfe6a07d6acbe199
Thibaut R <thibaut.rollos@filigran.io>
2024-05-30 11:39:01 +0200

    Added reconnectOnError handler for when facing READONLY errors

--- opencti-platform/opencti-graphql/src/database/redis.ts
+++ opencti-platform/opencti-graphql/src/database/redis.ts
@@ -109,19 +109,26 @@ const sentinelOptions = async (clusterNodes: Partial<SentinelAddress>[]): Promis
   };
 };
 
+export const reconnectOnError = (error: any) => {
+  const targetError = 'READONLY';
+  // Only reconnect when the error starts with "READONLY"
+  return error.message.slice(0, targetError.length) === targetError;
+};
+
 export const createRedisClient = async (provider: string, autoReconnect = false): Promise<Cluster | Redis> => {
   let client: Cluster | Redis;
   const redisMode: string = conf.get('redis:mode');
   const clusterNodes = generateClusterNodes(conf.get('redis:hostnames') ?? []);
+
   if (redisMode === 'cluster') {
     const clusterOpts = await clusterOptions();
     client = new Redis.Cluster(clusterNodes, clusterOpts);
   } else if (redisMode === 'sentinel') {
     const sentinelOpts = await sentinelOptions(clusterNodes);
-    client = new Redis(sentinelOpts);
+    client = new Redis({ ...sentinelOpts, reconnectOnError });
   } else {
     const singleOptions = await redisOptions(autoReconnect);
-    client = new Redis({ ...singleOptions, port: conf.get('redis:port'), host: conf.get('redis:hostname') });
+    client = new Redis({ ...singleOptions, port: conf.get('redis:port'), host: conf.get('redis:hostname'), reconnectOnError });
   }
 
   client.on('close', () => logApp.info(`[REDIS] Redis '${provider}' client closed`));
